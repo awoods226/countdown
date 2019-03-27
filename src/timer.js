b759590f5eaa33038ffbe36a86cdf61c3e8de892
Ari Woods <awoods226@gmail.com>
2019-03-26 20:50:29 -0500

    adding unit tests, splitting out logic

---
+++ src/timer.js
@@ -0,0 +1,109 @@
+import dateFns from "date-fns";
+
+export function parseTimeString(expiresAt, startDateTime) {
+  if (!expiresAt) {
+    console.log("ERROR: Expiration seed date required");
+    return;
+  }
+  var timeStringArray = expiresAt.split(",");
+
+  var now = startDateTime;
+  if (!now) {
+    now = new Date();
+  }
+  var currentDay = dateFns.getDay(now);
+  var localTimes = timeStringArray.map(f => {
+    var newDate = now;
+
+    //strip off duration
+    let splitArray = f.split("d");
+    let dateTime = splitArray[0];
+    let duration = splitArray[1].replace("d", "");
+
+    // parse string to a date object
+    var date = dateFns.parse(dateTime);
+    // strip off hours and minutes from desired date
+    var hours = dateFns.getHours(date);
+    var minutes = dateFns.getMinutes(date);
+
+    // calculate desired day of week
+    var targetDay = dateFns.getDay(date);
+    var distance = (targetDay + 7 - currentDay) % 7;
+    // set current date with desired day and time
+    newDate = dateFns.setDate(newDate, newDate.getDate() + distance);
+    newDate = dateFns.setHours(newDate, hours);
+    newDate = dateFns.setMinutes(newDate, minutes);
+    let eventDurationMinutes = parseInt(duration);
+    let startTime = dateFns.parse(newDate);
+    let endTime = dateFns.addMinutes(startTime, eventDurationMinutes);
+
+    // if distance is zero and event is over, set it to next week
+    if (distance === 0 && dateFns.isBefore(endTime, now)) {
+      newDate = dateFns.setDate(newDate, newDate.getDate() + 7);
+    }
+
+    return {
+      dateTime: dateFns.parse(newDate),
+      duration: eventDurationMinutes,
+      endTime: endTime
+    };
+  });
+  localTimes.sort((a, b) => {
+    var distancea = Math.abs(now - a.dateTime);
+    var distanceb = Math.abs(now - b.dateTime);
+    return distancea - distanceb;
+  });
+  return localTimes;
+}
+export function getTimeRemaining(startTime, endTimes, targetTime) {
+  if (!endTimes) {
+    return;
+  }
+  let now = startTime;
+  if (!now) {
+    now = new Date();
+  }
+  // remove any events that already happened today
+
+  let difference_ms = dateFns.differenceInMilliseconds(
+    targetTime.dateTime,
+    now
+  );
+  difference_ms = difference_ms / 1000;
+  var seconds = Math.floor(difference_ms % 60);
+  if (seconds === -0) {
+    seconds = 0;
+  }
+  difference_ms = difference_ms / 60;
+  var minutes = Math.floor(difference_ms % 60);
+  if (minutes === -0) {
+    minutes = 0;
+  }
+  difference_ms = difference_ms / 60;
+  var hours = Math.floor(difference_ms % 24);
+  var days = Math.floor(difference_ms / 24);
+  var remaining = {
+    days: minTwoDigits(days),
+    hours: minTwoDigits(hours),
+    minutes: minTwoDigits(minutes),
+    seconds: minTwoDigits(seconds),
+    total: difference_ms
+  };
+  return remaining;
+}
+export function isEventOngoing(startTime, endTime) {
+  let now = startTime;
+  if (!now) {
+    now = new Date();
+  }
+  if (dateFns.isBefore(now, endTime)) {
+    return true;
+  }
+  return false;
+}
+const minTwoDigits = n => {
+  if (n < 0) {
+    return n;
+  }
+  return (n < 10 ? "0" : "") + n;
+};
